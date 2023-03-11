b389c27ac96ea0dd48b7b94de01d33486fbcbf80
petrostrak <pit.trak@gmail.com>
2023-03-11 11:59:51 +0200

    Add test for blockchain

--- src/block.rs
+++ src/block.rs
@@ -92,3 +92,17 @@ impl Blockchain {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_blockchain() {
+        let mut b: Blockchain = Blockchain::new();
+        b.add_block("data".to_string());
+        b.add_block("data2".to_string());
+        b.add_block("data3".to_string());
+        dbg!(b);
+    }
+}
